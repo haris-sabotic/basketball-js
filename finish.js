cc73e8f8d26c6aa33a4c128e0d7cd3e55f0e6b8d
haris-sabotic <haris.sinfo16@gmail.com>
2024-02-23 10:12:09 +0100

    Load highscore from backend

--- finish.js
+++ finish.js
@@ -1,11 +1,5 @@
 const finish = () => {
-    // WS.close();
     document.querySelector("canvas").remove();
-
-    // World.clear(MATTER_ENGINE.world);
-    // Engine.clear(MATTER_ENGINE);
-    // PIXI_APP.ticker.stop();
-    // PIXI_APP.stage.destroy(true);
 
     SEND_WS_MESSAGE("finished", null);
 
@@ -13,25 +7,16 @@ const finish = () => {
         let message = JSON.parse(event.data);
 
         if (message.tag == "results") {
-            if (message)
+            if (message) {
                 document.querySelector(".score-value").innerHTML = `${message.score}`;
+                document.querySelector(".highscore-value").innerHTML = `${message.highscore}`;
+            }
         }
+
+        WS.close();
     });
 
     document.querySelector(".finish-container").classList.remove("hidden");
-
-
-    let highscore = localStorage.getItem("highscore");
-    if (highscore == null) {
-        highscore = 0;
-    }
-
-    if (score > highscore) {
-        localStorage.setItem("highscore", score);
-        highscore = score;
-    }
-
-    document.querySelector(".highscore-value").innerHTML = `${highscore}`;
 
     document.querySelector("#refresh").addEventListener("click", () => {
         location.reload();
