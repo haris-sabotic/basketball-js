18e687be4ae708d48dc0ad319df56c0101187b7d
haris-sabotic <haris.sinfo16@gmail.com>
2024-03-01 09:44:54 +0100

    Back to 10 points

--- main.js
+++ main.js
@@ -379,7 +379,7 @@ function runMain() {
                     RECORDING_BALL = [];
                     RECORDING_HOOP = [];
 
-                    if (score >= 1 && score < 20) {
+                    if (score >= 10 && score < 20) {
                         if (!hulkShownOnce) {
                             hulkShownOnce = true;
 
